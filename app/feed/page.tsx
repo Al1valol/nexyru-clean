25206fa5d30068d67a617550c3f70007283aae78
Max <max@Maxs-MacBook-Pro.local>
2026-05-10 18:12:16 -0700

    fix use client must be first line

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,6 +1,6 @@
+"use client";
 // @ts-nocheck
 export const dynamic = "force-dynamic";
-"use client";
 import { useState, useEffect, useRef, useCallback } from "react";
 import {
   getGlobalFeed, getFollowingFeed, getBigWinsFeed,
